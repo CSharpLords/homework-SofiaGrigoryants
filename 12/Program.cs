42fafeedd42f04bf23c337d0b0cda4fdb40b5f2c
SofiaGrigoryants <32613774+SofiaGrigoryants@users.noreply.github.com>
2017-12-24 11:37:19 +0300

    .

---
+++ 12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите расстояние до города №1");
+            string first = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №2");
+            string second = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №3");
+            string third = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №4");
+            string fourth = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №5");
+            string fifth = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №6");
+            string sixth = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №7");
+            string seventh = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №8");
+            string eighth = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №9");
+            string ninth = Console.ReadLine();
+            Console.WriteLine("Введите расстояние до города №10");
+            string tenth = Console.ReadLine();
+        }
+    }
+}
